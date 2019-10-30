84a08c435b0bd474906192e3dca7ac72ec9caa9d
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-10-30 15:55:43 +0000

    Added more examples of puppeteer

--- puppeteer_example.js
+++ puppeteer_example.js
@@ -1,25 +1,41 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-
-
-
-  try{
-    const browser = await puppeteer.launch({
-        headless: true
-    });
-    const page = await browser.newPage();
-    await page.goto('https://google.com/');
-    await page.type('input[name="q"]', 'Udemy Tutorials', {delay: 100});  // selector will probably change
-    await page.keyboard.press('Enter');
-    await page.waitForNavigation();
-    
-    await page.screenshot({path: 'example.png'});
   
-    await browser.close();
-  }catch(e){
-      console.log(e)
-  }
+  /* 1. Creating a PDF from the website */
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('https://learnscraping.com/');
+  await page.pdf({
+    path: './page.pdf',
+    format: 'A4'
+  })
+  await browser.close();
 
+
+  /* 2. Getting the URL or the Title of the current page */
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://learnscraping.com/');
+  
+  let title = await page.title();
+  console.log(`Title of the page is ${title}`);
+
+  let url = await page.url();
+  console.log(`URL of the page is ${url}`);
+
+  await browser.close();
+
+
+  /* 3. Emulate a phone */
+  const devices = require('puppeteer/DeviceDescriptors');
+
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.emulate(devices['iPhone X']);
+
+  await page.goto('https://learnscraping.com/');
+
+  await browser.close();
+  
 })();
-
